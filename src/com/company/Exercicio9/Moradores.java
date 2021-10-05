4cb917355cacaa35e346021293bc0d96761e0ba8
Bárbara Ann Lima <babiplima@gmail.com>
2021-10-05 09:41:51 -0300

    Criar Classe Morador

--- src/com/company/Exercicio9/Moradores.java
+++ src/com/company/Exercicio9/Moradores.java
@@ -1,9 +1,6 @@
 package com.company.Exercicio9;
 
 public class Moradores extends Pessoa {
-    // Atributos
-    private String telefone;
-    private double renda;
 
     //Métodos Construtores
 
@@ -11,40 +8,13 @@ public class Moradores extends Pessoa {
 
     }
 
-    public Moradores(String telefone, double renda) {
-        this.telefone = telefone;
-        this.renda = renda;
+    public Moradores(String nome) {
+        super(nome);
+
     }
 
-    public Moradores(String nome, String cpf, String telefone, double renda) {
+    public Moradores(String nome, String cpf) {
         super(nome, cpf);
-        this.telefone = telefone;
-        this.renda = renda;
-    }
 
-    //Métodos Getters e Setters
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public double getRenda() {
-        return renda;
-    }
-
-    public void setRenda(double renda) {
-        this.renda = renda;
-    }
-
-    public String toString() {
-        StringBuilder retorno = new StringBuilder();
-        retorno.append(super.toString());
-        retorno.append("Telefone " + telefone);
-        retorno.append("Renda R$ " + renda);
-        return retorno.toString();
     }
 }
